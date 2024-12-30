13967ebfe51e914ba228893c5da6f2bc1672f282
om <awsdevopsom@gmail.com>
2024-12-30 18:10:43 +0530

    om

---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "parameter" {
+    default =   [
+        {name = "dev.rds.username", type = "String", value = "admin1"}, 
+
+
+
+
+
+      #password  
+        {name = "dev.rds.password", type = "String", value = "ExpenseApp1234"},
+    ]  
+}
