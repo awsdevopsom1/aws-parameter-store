229786ae815930a22d930675a273191dc114c9d7
om <awsdevopsom@gmail.com>
2025-01-02 22:15:35 +0530

    om

--- vars.tf
+++ vars.tf
@@ -1,13 +1,13 @@
 variable "parameter" {
     default =   [
         {name = "dev.rds.username", type = "String", value = "admin1"}, 
-        {name = "dev.backend.DB_HOST", type = "String", value = "dev-mysql-rds.cluys2ag2dvp.us-east-1.rds.amazonaws.com"}
+        {name = "dev.backend.DB_HOST", type = "String", value = "dev-mysql-rds.cluys2ag2dvp.us-east-1.rds.amazonaws.com"},
 
 
 
 
 
       #password  
-        {name = "dev.rds.password", type = "String", value = "ExpenseApp1234"},
+      {name = "dev.rds.password", type = "String", value = "ExpenseApp1234"},
     ]  
 }
