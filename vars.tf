6a29d040e0a039523535afda3f37a0c1d57f222a
om <awsdevopsom@gmail.com>
2025-01-06 16:44:02 +0530

    m

--- vars.tf
+++ vars.tf
@@ -1,16 +1,17 @@
 variable "parameter" {
     default =   [
-        {name = "dev.backend.DB_HOST", type = "String", value = "dev-mysql-rds.cluys2ag2dvp.us-east-1.rds.amazonaws.com"},
+        # {name = "dev.backend.DB_HOST", type = "String", value = "dev-mysql-rds.cluys2ag2dvp.us-east-1.rds.amazonaws.com"},
         
 
         #RDS
-        {name = "dev.rds.username", type = "String", value = "admin1"}, 
-        {name = "dev.rds.endpoint", type = "String", value = "dev-mysql-rds.cluys2ag2dvp.us-east-1.rds.amazonaws.com"},  
+        # {name = "dev.rds.username", type = "String", value = "admin1"}, 
+        # {name = "dev.rds.endpoint", type = "String", value = "dev-mysql-rds.cluys2ag2dvp.us-east-1.rds.amazonaws.com"},  
 
 
 
 
       #password  
-      {name = "dev.rds.password", type = "String", value = "ExpenseApp1234"},
+      # {name = "dev.rds.password", type = "SecureString", value = "ExpenseApp1234"},
+      {name = "sonar.token", type="SecureString", value = "squ_1fc870152501ff56a28bb7658478476093721532" }
     ]  
 }
